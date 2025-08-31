159785336aa73f43a67873363e16ed739941bb56
Dylan Coon <Dylan@Mac.lan>
2025-08-31 14:53:19 -0400

    stashing changes

--- insight-service/controllers/controllers.go
+++ insight-service/controllers/controllers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"cloud.google.com/go/storage"
 	//"github.com/DylanCoon99/crypto_dashboard/crypto-service/api"
+	"github.com/DylanCoon99/crypto_dashboard/insight-service/cloud"
 )
 
 // stores configuration details; e.g. cloud client stuff
@@ -28,10 +29,17 @@ func (cfg *ApiCfg) Test(c *gin.Context) {
 
 
 
-func TestBucket(c *gin.Context) {
+func (cfg *ApiCfg) TestBucket(c *gin.Context) {
 
 	// pulls data from the cloud bucket for that coin name
 
+	coin_name := c.Param("coin_name")
 
+	data := cloud.ReadFromBucket(cfg.Client, coin_name, c.Request.Context())
+
+
+	c.JSON(http.StatusOK, gin.H{
+      "message": data,
+    })
 
 }
--- insight-service/main.go
+++ insight-service/main.go
@@ -1,20 +1,18 @@
 package main
 
-
 import (
+	"context"
 	"log"
 	"time"
-	"context"
-	//"net/http"	
+
+	//"net/http"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	//"github.com/joho/godotenv"
 	"cloud.google.com/go/storage"
 	"github.com/DylanCoon99/crypto_dashboard/insight-service/controllers"
 )
-
-
-
 
 func main() {
 
@@ -27,10 +25,9 @@ func main() {
 	}
 
 	// create the api config here
-	apiCfg := controllers.ApiCfg {
+	apiCfg := controllers.ApiCfg{
 		Client: client,
 	}
-
 
 	// gin server setup
 	r := gin.Default()
@@ -44,22 +41,18 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-
 	api := r.Group("/api")
 	{
 		//test endpoint
 		api.GET("/test", apiCfg.Test)
 
 		// add other endpoints
-		api.GET("/testbucket/:coin_name", controllers.TestBucket)
+		api.GET("/testbucket/:coin_name", apiCfg.TestBucket)
 
 	}
-
 
 	log.Println("Server starting on port 8080...")
 
 	log.Fatal(r.Run(":8080"))
-
-
 
 }
